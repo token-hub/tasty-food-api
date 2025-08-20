6736a7812db669ef3348e09faa742274581f1091
John <johnsuyang2118@gmail.com>
2025-08-21 04:59:24 +0800

    import mongoose and add recipe initial route

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 import express from "express";
 import cors from "cors";
 
+// utils
+import "./utils/db/db.js";
+import "./utils/db/mongoose.js";
+
+import recipeRoutes from "./modules/recipe/index.js";
+
 const app = express();
+const router = express.Router();
 
 // Middleware configuration
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+
+// routes
+app.use("/api/recipes", () => recipeRoutes(router));
 
 export default app;
