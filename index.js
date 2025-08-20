5132c804cf1f9bcb2bee826dbf432463db07a463
John <johnsuyang2118@gmail.com>
2025-08-21 00:56:46 +0800

    add express and create initial server

---
+++ index.js
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
