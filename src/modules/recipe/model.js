b67f4844f86b0be4ba89ffd8aaa25ce29d62e55b
John <johnsuyang2118@gmail.com>
2025-08-22 00:24:32 +0800

    update image and video to have a default value of null and enable timestamp

--- src/modules/recipe/model.js
+++ src/modules/recipe/model.js
@@ -1,151 +1,155 @@
 import mongoose, { Schema } from "mongoose";
 
-const RecipeSchema = new Schema({
-    author: {
-        name: {
-            type: String,
-            required: true
-        },
-        userId: { type: mongoose.Schema.Types.ObjectId, required: true }
-    },
-    ingredients: [
-        {
+const RecipeSchema = new Schema(
+    {
+        author: {
             name: {
                 type: String,
-                trim: true,
-                lowercase: true,
                 required: true
             },
-            quantity: {
-                type: Number,
-                required: true
-            },
-            unit: {
-                type: String,
-                required: true
-            },
-            isMain: {
-                type: Boolean,
-                default: false
+            userId: { type: mongoose.Schema.Types.ObjectId, required: true }
+        },
+        ingredients: [
+            {
+                name: {
+                    type: String,
+                    trim: true,
+                    lowercase: true,
+                    required: true
+                },
+                quantity: {
+                    type: Number,
+                    required: true
+                },
+                unit: {
+                    type: String,
+                    required: true
+                },
+                isMain: {
+                    type: Boolean,
+                    default: false
+                }
             }
-        }
-    ],
-    prepTime: {
-        hours: {
-            type: Number,
-            default: 0
-        },
-        minutes: {
-            type: Number,
-            required: true
-        }
-    },
-    cookTime: {
-        hours: {
-            type: Number,
-            default: 0
-        },
-        minutes: {
-            type: Number,
-            required: true
-        }
-    },
-    name: {
-        type: String,
-        trim: true,
-        lowercase: true,
-        required: true
-    },
-    description: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    categories: [String],
-    image: {
-        name: String,
-        size: String,
-        link: String
-    },
-    video: {
-        name: String,
-        length: String,
-        size: String,
-        link: String
-    },
-    totalRatings: {
-        type: Number,
-        default: 0
-    },
-    numberOfRatings: {
-        type: Number,
-        default: 0
-    },
-    isArchive: {
-        type: Boolean,
-        default: false
-    },
-    goodForPeopleCount: {
-        type: Number,
-        default: 1
-    },
-    topFiveRecentRatings: [
-        {
-            ratingsId: {
-                type: mongoose.Schema.Types.ObjectId,
-                required: true
-            },
-            comment: {
-                type: String,
-                trim: true,
-                required: true
-            },
-            rating: {
+        ],
+        prepTime: {
+            hours: {
                 type: Number,
                 default: 0
             },
-            rater: {
-                raterId: {
+            minutes: {
+                type: Number,
+                required: true
+            }
+        },
+        cookTime: {
+            hours: {
+                type: Number,
+                default: 0
+            },
+            minutes: {
+                type: Number,
+                required: true
+            }
+        },
+        name: {
+            type: String,
+            trim: true,
+            lowercase: true,
+            required: true
+        },
+        description: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        categories: [String],
+        image: {
+            type: {
+                name: { type: String, required: true },
+                size: { type: String, required: true },
+                link: { type: String, required: true }
+            },
+            default: null
+        },
+        video: {
+            type: {
+                name: { type: String, required: true },
+                length: { type: String, required: true },
+                size: { type: String, required: true },
+                link: { type: String, required: true }
+            },
+            default: null
+        },
+        totalRatings: {
+            type: Number,
+            default: 0
+        },
+        numberOfRatings: {
+            type: Number,
+            default: 0
+        },
+        isArchive: {
+            type: Boolean,
+            default: false
+        },
+        goodForPeopleCount: {
+            type: Number,
+            default: 1
+        },
+        topFiveRecentRatings: [
+            {
+                ratingsId: {
                     type: mongoose.Schema.Types.ObjectId,
                     required: true
                 },
-                name: {
+                comment: {
                     type: String,
+                    trim: true,
+                    required: true
+                },
+                rating: {
+                    type: Number,
+                    default: 0
+                },
+                rater: {
+                    raterId: {
+                        type: mongoose.Schema.Types.ObjectId,
+                        required: true
+                    },
+                    name: {
+                        type: String,
+                        required: true
+                    }
+                },
+                likes: [mongoose.Schema.Types.ObjectId],
+                createdAt: {
+                    type: Number,
+                    default: Date.now,
                     required: true
                 }
-            },
-            likes: [mongoose.Schema.Types.ObjectId],
-            createdAt: {
-                type: Number,
-                default: Date.now,
-                required: true
             }
-        }
-    ],
-    createdAt: {
-        type: Number,
-        default: Date.now,
-        required: true
-    }
-    // topRatedIndex: {
-    //     type: String,
-    //     index: true,
-    //     unique: true,
-    //     required: true
-    // },
-    // categoriesIndex: {
-    //     type: String,
-    //     index: true,
-    //     unique: true,
-    //     required: true
-    // },
-    // ownRecipeIndex: {
-    //     type: String,
-    //     index: true,
-    //     unique: true,
-    //     required: true
-    // }
-});
+        ]
+        // topRatedIndex: {
+        //     type: String,
+        //     index: true,
+        //     unique: true,
+        //     required: true
+        // },
+        // categoriesIndex: {
+        //     type: String,
+        //     index: true,
+        //     unique: true,
+        //     required: true
+        // },
+        // ownRecipeIndex: {
+        //     type: String,
+        //     index: true,
+        //     unique: true,
+        //     required: true
+        // }
+    },
+    { timestamps: true }
+);
 
 RecipeSchema.index({ totalRatings: -1, createdAt: -1 });
 RecipeSchema.index({ "author.userId": -1, createdAt: -1 });
