986b5cf319e00614ab9fa18aa23063882e999322
John <johnsuyang2118@gmail.com>
2025-08-21 22:46:34 +0800

    add database to use

--- src/configs/mongodb.js
+++ src/configs/mongodb.js
@@ -1,5 +1,5 @@
 const config = {
-    uri: `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@localhost:${process.env.MONGODB_PORT}/?authMechanism=DEFAULT`,
+    uri: `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@127.0.0.1:${process.env.MONGODB_PORT}/?authMechanism=DEFAULT&authSource=${process.env.MONGODB_DATABASE}`,
     database: process.env.MONGODB_DATABASE
 };
 
