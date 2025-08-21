c6ad6b093a99e66fc8505fc17418a50b9272f2b8
John <johnsuyang2118@gmail.com>
2025-08-22 01:13:04 +0800

    add getAllRecipes and add constructor

--- src/modules/recipe/service.js
+++ src/modules/recipe/service.js
@@ -1,13 +1,27 @@
 import RecipeModel from "./model.js";
 import { ObjectId } from "mongodb";
 class RecipeService {
+    #model;
+
+    constructor() {
+        this.#model = RecipeModel;
+    }
+
+    get model() {
+        return this.#model;
+    }
+
+    getAllRecipes() {
+        return this.model.find();
+    }
+
     createRecipe(data) {
         // implement business logic
         if (data?.author?.userId) {
             data.author.userId = new ObjectId(data.author.userId);
         }
 
-        return RecipeModel.create(data);
+        return this.model.create(data);
     }
 }
 
