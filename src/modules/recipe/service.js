df0d155c4f9560a1c6b647fbc9fe45a3576d526d
John <johnsuyang2118@gmail.com>
2025-08-21 22:16:23 +0800

    convert the author userId to objectId before storing

--- src/modules/recipe/service.js
+++ src/modules/recipe/service.js
@@ -1,8 +1,12 @@
 import RecipeModel from "./model.js";
-
+import { ObjectId } from "mongodb";
 class RecipeService {
     createRecipe(data) {
         // implement business logic
+        if (data?.author?.userId) {
+            data.author.userId = new ObjectId(data.author.userId);
+        }
+
         return RecipeModel.create(data);
     }
 }
