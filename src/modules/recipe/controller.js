4548b623bae7a7724a272e0352d24459b38ed3c2
John <johnsuyang2118@gmail.com>
2025-08-21 21:31:05 +0800

    handle errors and return the same instance of the controller

--- src/modules/recipe/controller.js
+++ src/modules/recipe/controller.js
@@ -1,20 +1,32 @@
 import RecipeService from "./service.js";
 
-async function getAllRecipes(req, res) {
-    try {
-        const userData = req.body;
-        const service = new RecipeService();
-        const newRecipe = await service.createRecipe(userData);
-        res.status(200).json({
-            status: "success",
-            message: "recipe created successfully.",
-            data: {
-                newRecipe
-            }
-        });
-    } catch (error) {
-        console.log(error);
+class RecipeController {
+    async getAllRecipes(req, res) {
+        try {
+            const userData = req.body;
+            const service = new RecipeService();
+            const newRecipe = await service.createRecipe(userData);
+            res.status(200).json({
+                status: "success",
+                message: "recipe created successfully.",
+                data: {
+                    newRecipe
+                }
+            });
+        } catch (error) {
+            console.log(error);
+            const errors = Object.entries(error.errors).map((err) => {
+                return {
+                    [err[0]]: err[1].message
+                };
+            });
+
+            return res.status(422).json({
+                error: error._message,
+                details: errors
+            });
+        }
     }
 }
 
-export default getAllRecipes;
+export default new RecipeController();
