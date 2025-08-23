87cd96c5595f1604810d58208416219dba659943
John <johnsuyang2118@gmail.com>
2025-08-24 05:03:45 +0800

    add route for getting the rate of current user for the recipe he/she viewing

--- src/modules/rating/index.js
+++ src/modules/rating/index.js
@@ -5,5 +5,6 @@ import RatingController from "./controller.js";
 
 router.post("/", RatingController.createRating);
 router.get("/", RatingController.getAllRating);
+router.get("/:recipeId/:raterId", RatingController.getRating);
 
 export default router;
