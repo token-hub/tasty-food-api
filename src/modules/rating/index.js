e356bf01b5c914d610af71ea560e79f6bf133f05
John <johnsuyang2118@gmail.com>
2025-08-24 06:02:06 +0800

    add route for like/unliking rating

--- src/modules/rating/index.js
+++ src/modules/rating/index.js
@@ -6,5 +6,6 @@ import RatingController from "./controller.js";
 router.post("/", RatingController.createRating);
 router.get("/", RatingController.getAllRating);
 router.get("/:recipeId/:raterId", RatingController.getRating);
+router.put("/:ratingId/:likerId", RatingController.likeUnlikeRating);
 
 export default router;
