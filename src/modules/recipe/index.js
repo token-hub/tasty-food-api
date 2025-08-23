0127347b7e4c015d9918367e9e7820010442e47e
John <johnsuyang2118@gmail.com>
2025-08-23 23:23:45 +0800

    move the router instance directly in file instead of passing it

--- src/modules/recipe/index.js
+++ src/modules/recipe/index.js
@@ -1,11 +1,10 @@
+import express from "express";
+const router = express.Router();
 import RecipeController from "./controller.js";
 
-function recipeRoutes(router) {
-    router.get("/", (req, res) => RecipeController.getAllRecipes(req, res));
-    router.post("/", (req, res) => RecipeController.createRecipe(req, res));
-    router.get("/:recipeId", (req, res) => RecipeController.getRecipe(req, res));
-    router.put("/:recipeId", (req, res) => RecipeController.updateRecipe(req, res));
-    return router;
-}
+router.get("/", (req, res) => RecipeController.getAllRecipes(req, res));
+router.post("/", (req, res) => RecipeController.createRecipe(req, res));
+router.get("/:recipeId", (req, res) => RecipeController.getRecipe(req, res));
+router.put("/:recipeId", (req, res) => RecipeController.updateRecipe(req, res));
 
-export default recipeRoutes;
+export default router;
