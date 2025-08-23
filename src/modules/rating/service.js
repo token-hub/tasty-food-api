7ab377fd94422e8aa22da933d198dd7959fed919
John <johnsuyang2118@gmail.com>
2025-08-24 02:01:38 +0800

    create getAllRating with support for pagination

--- src/modules/rating/service.js
+++ src/modules/rating/service.js
@@ -3,6 +3,13 @@ import { ObjectId } from "mongodb";
 
 class RatingService {
     #model;
+    #pagination = {
+        page: 1,
+        cursor: "",
+        limit: 6,
+        sortBy: "updatedAt",
+        order: -1
+    };
 
     constructor() {
         this.#model = RatingModel;
@@ -12,7 +19,38 @@ class RatingService {
         return this.#model;
     }
 
+    set paginationData(data) {
+        this.#pagination = { ...this.#pagination, ...data };
+    }
+
+    get paginationData() {
+        return this.#pagination;
+    }
+
+    transfromData(data) {
+        data.recipeId = new ObjectId(data.recipeId);
+        return data;
+    }
+
+    getQueryAllRating(data) {
+        let query = { recipeId: data.recipeId };
+        const { limit, page, cursor, order } = this.paginationData;
+        let skip = limit * (page - 1) > 1 ? limit * (page - 1) : 1;
+
+        if (order == 1) {
+            skip = skip - 1;
+        }
+
+        if (cursor) {
+            query.updatedAt = { [order == -1 ? "$lte" : "$gte"]: new Date(cursor) };
+        }
+
+        return { query, skip };
+    }
+
     createRating(data) {
+        // add validation
+
         if (!data) {
             throw new Error("Missing required Data");
         }
@@ -25,7 +63,31 @@ class RatingService {
             data.rater.raterId = new ObjectId(data?.rater.raterId);
         }
 
+        // make the data.comment only required if the rating is below 4
+        // notify the author here
+
         return this.model.create(data);
+    }
+
+    async getAllRating(data) {
+        this.paginationData = data.pagination;
+        delete data.pagination;
+        // add validation
+
+        data = this.transfromData(data);
+        const { query, skip } = this.getQueryAllRating(data);
+        const { sortBy, order, limit, page } = this.paginationData;
+
+        const ratings = await this.model
+            .find(query)
+            .skip(skip)
+            .sort({ [sortBy]: order })
+            .limit(limit);
+
+        return {
+            ratings,
+            page
+        };
     }
 }
 
