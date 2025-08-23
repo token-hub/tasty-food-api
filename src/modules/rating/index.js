801a986c9980b9d0ccf5efaab10e6f5fd656d293
John <johnsuyang2118@gmail.com>
2025-08-24 02:02:49 +0800

    add new route for getting ratings of a recipe

--- src/modules/rating/index.js
+++ src/modules/rating/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 import RatingController from "./controller.js";
 
 router.post("/", RatingController.createRating);
+router.get("/", RatingController.getAllRating);
 
 export default router;
