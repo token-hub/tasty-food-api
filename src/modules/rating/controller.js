ea4906859dd0078bf72bbff21cdb89c3914950fb
John <johnsuyang2118@gmail.com>
2025-08-23 23:57:57 +0800

    add response if success and add error handler if there's multiple

--- src/modules/rating/controller.js
+++ src/modules/rating/controller.js
@@ -14,9 +14,24 @@ class RatingController {
     async createRating(req, res) {
         try {
             const data = req.body;
-            await this.service.createRating(data);
+            const rating = await this.service.createRating(data);
+
+            return res.status(201).json({
+                status: "Success",
+                details: rating
+            });
         } catch (error) {
-            console.log(error);
+            if (error.errors) {
+                const errors = Object.entries(error.errors).map((err) => {
+                    return {
+                        [err[0]]: err[1].message
+                    };
+                });
+                return res.status(422).json({
+                    error: error._message,
+                    details: errors
+                });
+            }
 
             return res.status(422).json({
                 error: error.message || "something went wrong"
