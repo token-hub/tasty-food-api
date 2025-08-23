778f1cde23593a059f3a7082bc34b1279f16c993
John <johnsuyang2118@gmail.com>
2025-08-24 00:51:41 +0800

    just pass the function as reference to avoid repeating the req, res

--- src/modules/recipe/index.js
+++ src/modules/recipe/index.js
@@ -2,9 +2,9 @@ import express from "express";
 const router = express.Router();
 import RecipeController from "./controller.js";
 
-router.get("/", (req, res) => RecipeController.getAllRecipes(req, res));
-router.post("/", (req, res) => RecipeController.createRecipe(req, res));
-router.get("/:recipeId", (req, res) => RecipeController.getRecipe(req, res));
-router.put("/:recipeId", (req, res) => RecipeController.updateRecipe(req, res));
+router.get("/", RecipeController.getAllRecipes);
+router.post("/", RecipeController.createRecipe);
+router.get("/:recipeId", RecipeController.getRecipe);
+router.put("/:recipeId", RecipeController.updateRecipe);
 
 export default router;
