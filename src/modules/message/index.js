a390f561c04e12814a635ecfcf8152a22aeed84d
John <johnsuyang2118@gmail.com>
2025-08-28 05:32:43 +0800

    add create message route

--- src/modules/message/index.js
+++ src/modules/message/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 import MessageController from "./controller.js";
 
 router.get("/", MessageController.getMessages);
+router.post("/", MessageController.createMessage);
 
 export default router;
