7d078da7a7ed2d41ac7ef0bd1aa2fbdc32f6001d
John <johnsuyang2118@gmail.com>
2025-08-29 06:45:08 +0800

    add route for getting unread notifications count for a user

--- src/modules/notification/index.js
+++ src/modules/notification/index.js
@@ -5,5 +5,6 @@ import NotificationController from "./controller.js";
 
 router.post("/", NotificationController.createNotification);
 router.get("/", NotificationController.getNotifications);
+router.get("/:userId", NotificationController.getUnreadNotificationsCount);
 
 export default router;
