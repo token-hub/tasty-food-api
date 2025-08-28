24b7f163a2a8b6d6510b40b547d415150633445f
John <johnsuyang2118@gmail.com>
2025-08-29 07:28:35 +0800

    add route for marking all the unread notifion of a user

--- src/modules/notification/index.js
+++ src/modules/notification/index.js
@@ -7,5 +7,6 @@ router.post("/", NotificationController.createNotification);
 router.get("/", NotificationController.getNotifications);
 router.get("/:userId", NotificationController.getUnreadNotificationsCount);
 router.put("/:notificationId", NotificationController.updateNotificationIsRead);
+router.put("/markAllUnread/:userId", NotificationController.markAllUnReadNotifToRead);
 
 export default router;
