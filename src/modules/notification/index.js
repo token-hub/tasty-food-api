b505c341730ee9c3522c548ff1c308d4b99f4dc6
John <johnsuyang2118@gmail.com>
2025-08-29 06:34:44 +0800

    add a route for getting notifications

--- src/modules/notification/index.js
+++ src/modules/notification/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 import NotificationController from "./controller.js";
 
 router.post("/", NotificationController.createNotification);
+router.get("/", NotificationController.getNotifications);
 
 export default router;
