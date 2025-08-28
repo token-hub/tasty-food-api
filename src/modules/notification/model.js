5a743424cc6af25573c1829061d5a7b6bb60d4bf
John <johnsuyang2118@gmail.com>
2025-08-29 05:25:55 +0800

    create notification model

---
+++ src/modules/notification/model.js
@@ -0,0 +1,22 @@
+import mongoose, { Schema } from "mongoose";
+
+const NotificationSchema = new Schema(
+    {
+        subject: { type: String, required: true },
+        recipe: {
+            name: { type: String, required: true },
+            imageLink: { type: String, required: true }
+        },
+        title: { type: String, required: true },
+        description: { type: String, required: true },
+        userId: { type: Schema.Types.ObjectId, required: true },
+        isRead: { type: Boolean, default: false },
+        link: { type: String, required: true }
+    },
+    { timestamps: true }
+);
+NotificationSchema.index({ userId: -1, isRead: -1, updatedAt: -1 });
+
+const NotificationModel = mongoose.model("Notification", NotificationSchema);
+
+export default NotificationModel;
