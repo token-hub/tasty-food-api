0fc72422d0110cae9d32675d9f372738af1f9ff0
John <johnsuyang2118@gmail.com>
2025-08-29 23:03:20 +0800

    add get reports method

--- src/modules/report/controller.js
+++ src/modules/report/controller.js
@@ -13,11 +13,24 @@ class ReportController {
 
     createReport = async (req, res, next) => {
         try {
-            const userData = req.body;
-            const newReport = await this.service.createReport(userData);
+            const data = req.body;
+            const newReport = await this.service.createReport(data);
             return res.status(201).json({
                 status: "Success",
                 details: newReport
+            });
+        } catch (error) {
+            next(error);
+        }
+    };
+
+    getReports = async (req, res, next) => {
+        try {
+            const data = req.body;
+            const reports = await this.service.getReports(data);
+            return res.status(200).json({
+                status: "Success",
+                details: reports
             });
         } catch (error) {
             next(error);
