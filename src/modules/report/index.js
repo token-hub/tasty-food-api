cdc9ce97f6b618b25ad1de1831c5b46f86457ed6
John <johnsuyang2118@gmail.com>
2025-08-29 23:03:53 +0800

    add get reports route

--- src/modules/report/index.js
+++ src/modules/report/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 import ReportController from "./controller.js";
 
 router.post("/", ReportController.createReport);
+router.get("/", ReportController.getReports);
 
 export default router;
