c924d8f7426bad129d4f1e08ec65061898ca012e
John <johnsuyang2118@gmail.com>
2025-08-31 07:17:39 +0800

    create user routes

---
+++ src/modules/user/index.js
@@ -0,0 +1,8 @@
+import express from "express";
+import UserController from "./controller.js";
+
+const router = express.Router();
+
+router.post("/", UserController.createUser);
+
+export default router;
