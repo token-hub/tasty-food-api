ba8457f4d71b302105218c769af0cc71103c1344
John <johnsuyang2118@gmail.com>
2025-09-01 03:07:52 +0800

    add update user route

--- src/modules/user/index.js
+++ src/modules/user/index.js
@@ -4,5 +4,6 @@ import UserController from "./controller.js";
 const router = express.Router();
 
 router.post("/", UserController.createUser);
+router.put("/:userId", UserController.updateUser);
 
 export default router;
