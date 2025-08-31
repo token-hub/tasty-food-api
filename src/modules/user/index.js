4f0523b3e7f3c1e3aec3ddbb15293e1fad35e5cf
John <johnsuyang2118@gmail.com>
2025-09-01 03:14:14 +0800

    add get a single user route

--- src/modules/user/index.js
+++ src/modules/user/index.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post("/", UserController.createUser);
 router.put("/:userId", UserController.updateUser);
+router.get("/:userId", UserController.getUser);
 
 export default router;
