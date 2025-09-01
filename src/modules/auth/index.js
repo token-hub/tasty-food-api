a9c313bcd1615fd5b21f8b979b7a933d4172c19f
John <johnsuyang2118@gmail.com>
2025-09-02 06:06:14 +0800

    create route for signing up users

---
+++ src/modules/auth/index.js
@@ -0,0 +1,7 @@
+import express from "express";
+import AuthController from "./controller.js";
+const router = express.Router();
+
+router.post("/signUp", AuthController.signUp);
+
+export default router;
