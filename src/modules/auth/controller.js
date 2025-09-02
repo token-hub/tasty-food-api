8be9097e262c72c6c450070a42bd1977d5678d8c
John <johnsuyang2118@gmail.com>
2025-09-03 01:50:17 +0800

    add login controller method

--- src/modules/auth/controller.js
+++ src/modules/auth/controller.js
@@ -23,6 +23,20 @@ class AuthController {
             next(error);
         }
     };
+
+    login = async (req, res, next) => {
+        try {
+            const data = req.body;
+            const headers = req.headers;
+            const result = await this.service.login(data, headers);
+            return res.status(200).json({
+                status: "Success",
+                details: result
+            });
+        } catch (error) {
+            next(error);
+        }
+    };
 }
 
 export default new AuthController();
