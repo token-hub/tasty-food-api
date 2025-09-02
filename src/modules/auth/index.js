3fe7f3df6313a8bad16b920b63a2ceef5de84c70
John <johnsuyang2118@gmail.com>
2025-09-03 01:50:24 +0800

    add route for login

--- src/modules/auth/index.js
+++ src/modules/auth/index.js
@@ -3,5 +3,6 @@ import AuthController from "./controller.js";
 const router = express.Router();
 
 router.post("/signUp", AuthController.signUp);
+router.post("/login", AuthController.login);
 
 export default router;
