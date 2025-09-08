cb289373cb3867462582aa3b069a2aaa6bfc2eff
John <johnsuyang2118@gmail.com>
2025-09-08 22:16:14 +0800

    make the getRecipes a post request due to having request body

--- src/modules/recipe/index.js
+++ src/modules/recipe/index.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 import RecipeController from "./controller.js";
 
-router.get("/", RecipeController.getAllRecipes);
+router.post("/getRecipes", RecipeController.getAllRecipes);
 router.post("/", RecipeController.createRecipe);
 router.get("/:recipeId", RecipeController.getRecipe);
 router.put("/:recipeId", RecipeController.updateRecipe);
