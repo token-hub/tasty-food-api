50b7de0b946678cb1d28d5533c8de6f47c4d854f
John <johnsuyang2118@gmail.com>
2025-09-08 22:15:14 +0800

    restrict update to the author of the recipe only, update queryAllRecipe to be able to fetch recipes matching only the author id, and add projection for getting a recipe

--- src/modules/recipe/service.js
+++ src/modules/recipe/service.js
@@ -1,8 +1,10 @@
 import RecipeModel from "./model.js";
 import { ObjectId } from "mongodb";
+import AuthService from "../auth/service.js";
 
 class RecipeService {
     #model;
+    #authService;
     #pagination = {
         page: 1,
         cursor: "",
@@ -13,6 +15,11 @@ class RecipeService {
 
     constructor() {
         this.#model = RecipeModel;
+        this.#authService = new AuthService();
+    }
+
+    get authService() {
+        return this.#authService;
     }
 
     get model() {
@@ -39,7 +46,6 @@ class RecipeService {
 
     getQueryAllRecipes(data) {
         const { limit, cursor, page, order } = this.paginationData;
-        const { filters } = data;
 
         let defaultSkip = limit * (page - 1);
         let isNotFirstPage = defaultSkip > 0;
@@ -59,8 +65,12 @@ class RecipeService {
             query.updatedAt = { [order == -1 ? "$lte" : "$gte"]: new Date(cursor) };
         }
 
-        if (filters?.categories.length) {
-            query.categories = { $in: filters.categories };
+        if (data?.author) {
+            query["author.userId"] = { $eq: data?.author?.userId };
+        }
+
+        if (data?.filters?.categories.length) {
+            query.categories = { $in: data?.filters.categories };
         }
 
         return { query, skip };
@@ -71,6 +81,8 @@ class RecipeService {
 
         this.paginationData = data.pagination;
         delete data.pagination;
+
+        this.transformData(data);
 
         const { query, skip } = this.getQueryAllRecipes(data);
         const { sortBy, limit, order, page } = this.paginationData;
@@ -96,7 +108,7 @@ class RecipeService {
             throw new Error("Recipe Id is missing");
         }
         this.transformData(data);
-        return this.model.findById({ _id: data.recipeId });
+        return this.model.findById({ _id: data.recipeId }, { totalRating: 0, isArchive: 0, __v: 0 });
     }
 
     createRecipe(data) {
@@ -106,13 +118,25 @@ class RecipeService {
         return this.model.create(data);
     }
 
-    updateRecipe(data) {
+    async isRecipeAuthor(recipeId, headers) {
+        const session = await this.authService.getSession(headers);
+        const recipe = await this.getRecipe({ recipeId });
+        const isRecipeAuthor = recipe.author.userId.equals(session?.user?.id);
+
+        if (!isRecipeAuthor) {
+            throw new Error("Action forbidden");
+        }
+    }
+
+    async updateRecipe(data, headers) {
         if (!data.recipeId) {
             throw new Error("Recipe Id is missing");
         }
 
         this.transformData(data);
         const recipeId = data.recipeId;
+        await this.isRecipeAuthor(recipeId, headers);
+
         delete data.recipeId;
 
         return this.model.findByIdAndUpdate(
