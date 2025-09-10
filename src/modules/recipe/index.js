8bc38c2a74dacc5e92e43a1f6df2d5bb5173654e
John <johnsuyang2118@gmail.com>
2025-09-10 22:27:41 +0800

    add a route for getting total recipes

--- src/modules/recipe/index.js
+++ src/modules/recipe/index.js
@@ -6,5 +6,6 @@ router.post("/getRecipes", RecipeController.getAllRecipes);
 router.post("/", RecipeController.createRecipe);
 router.get("/:recipeId", RecipeController.getRecipe);
 router.put("/:recipeId", RecipeController.updateRecipe);
+router.get("/getRecipes/totalCount", RecipeController.getTotalRecipesCount);
 
 export default router;
