398ef7a48747497bfcfd1758138b665c56c64cd9
John <johnsuyang2118@gmail.com>
2025-09-16 21:34:42 +0800

    change getRatings to post request due to having formbody and add route for getting total ratings count

--- src/modules/rating/index.js
+++ src/modules/rating/index.js
@@ -4,8 +4,9 @@ const router = express.Router();
 import RatingController from "./controller.js";
 
 router.post("/", RatingController.createRating);
-router.get("/", RatingController.getAllRating);
+router.post("/getRatings", RatingController.getAllRating);
 router.get("/:recipeId/:raterId", RatingController.getRating);
+router.get("/:recipeId/getRatings/totalCount", RatingController.getRatingsTotalCount);
 router.put("/:ratingId/:likerId", RatingController.likeUnlikeRating);
 
 export default router;
