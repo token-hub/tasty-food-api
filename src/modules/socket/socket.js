9efedbf4354706690d6386a0c8309bd1626c004e
John <johnsuyang2118@gmail.com>
2025-10-06 02:08:17 +0800

    store authenticated users after login

--- src/modules/socket/socket.js
+++ src/modules/socket/socket.js
@@ -1,9 +1,21 @@
 import { Server } from "socket.io";
 
+const authenticatedUsers = [];
+
 export function createSocket(server) {
     const io = new Server(server, { cors: { origin: process.env.CLIENT_URL, methods: ["GET", "POST"] } });
 
     io.on("connection", (socket) => {
-        console.log(socket.id); // ojIckSD2jqNzOqIrAGzL
+        socket.on("get-users", () => {
+            socket.emit("users", authenticatedUsers);
+        });
+
+        socket.on("authenticate", (data) => {
+            console.log("Authenticate event received:", data);
+            if (data.id && data.socketId) {
+                authenticatedUsers.push({ id: data.id, socketId: data.socketId });
+                socket.broadcast.emit("users", authenticatedUsers);
+            }
+        });
     });
 }
