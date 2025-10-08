2654661f76f60e7b5637395eebf13ef093714592
John <johnsuyang2118@gmail.com>
2025-10-09 06:44:19 +0800

    update socketId as user reconnect with userId

--- src/modules/socket/socket.js
+++ src/modules/socket/socket.js
@@ -4,16 +4,35 @@ let authenticatedUsers = [];
 
 export function createSocket(server) {
     const io = new Server(server, { cors: { origin: process.env.CLIENT_URL, methods: ["GET", "POST"] } });
-
     io.on("connection", (socket) => {
+        const userId = socket.handshake.auth.userId;
+
+        if (userId) {
+            console.log("New connection:", socket.id);
+            const exist = authenticatedUsers.some((user) => userId === user.id);
+            if (exist) {
+                authenticatedUsers = authenticatedUsers.map((user) => {
+                    if (userId === user.id) {
+                        return { ...user, socketId: socket.id };
+                    } else {
+                        return user;
+                    }
+                });
+            } else {
+                authenticatedUsers.push({ id: userId, socketId: socket.id });
+            }
+
+            console.log(authenticatedUsers);
+        }
+
         socket.on("get-users", () => {
             socket.emit("users", authenticatedUsers);
         });
 
         socket.on("authenticate", (data) => {
             console.log("Authenticate event received:", data);
-            if (data.id && data.socketId) {
-                authenticatedUsers.push({ id: data.id, socketId: data.socketId });
+            if (data.id) {
+                authenticatedUsers.push({ id: data.id, socketId: socket.id });
                 socket.broadcast.emit("users", authenticatedUsers);
             }
         });
@@ -28,7 +47,7 @@ export function createSocket(server) {
             console.log("Private Message:", data);
             const targetUser = authenticatedUsers.find((user) => user.id === data.to);
             if (targetUser) {
-                io.to(targetUser.socketId).emit("private-message", data.message);
+                io.to(targetUser.socketId).emit("private-message", data);
             } else {
                 console.log("Target user not found:", data.to);
             }
