ceadc302f5a7a5539300455820cbbed75b09d685
John <johnsuyang2118@gmail.com>
2025-10-09 06:44:39 +0800

    re-throw error when occured

--- src/utils/session.js
+++ src/utils/session.js
@@ -4,11 +4,12 @@ export const sessionWrapper = async (cb) => {
     const session = await mongoose.startSession();
     try {
         const result = await session.withTransaction(async () => {
-            return cb(session);
+            return await cb(session);
         });
         return result;
     } catch (error) {
         console.error("Transaction aborted:", error);
+        throw error;
     } finally {
         session.endSession();
     }
